91d8b3ddf1635c0c25b44125498a837ed8393e59
inhwanK <inhan1009@naver.com>
2022-03-12 01:37:28 +0900

    first commit : KISS - Keep It Simpl Stupid!

---
+++ src/main/java/org/deco/gachicoding/GachicodingApplication.java
@@ -0,0 +1,12 @@
+package org.deco.gachicoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachicodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GachicodingApplication.class, args);
+    }
+}
---
+++ src/test/java/org/deco/gachicoding/GachicodingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deco.gachicoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachicodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
