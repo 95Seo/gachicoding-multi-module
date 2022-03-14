0bac35fbb133d828a9183c466500b7e2eb7ee519
inhwanK <inhan1009@naver.com>
2022-03-14 21:28:29 +0900

    Feat(User) : 추가 User 테이블 해당하는 칼럼 변수

--- src/main/java/org/deco/gachicoding/domain/user/User.java
+++ src/main/java/org/deco/gachicoding/domain/user/User.java
@@ -1,7 +1,14 @@
 package org.deco.gachicoding.domain.user;
 
-import javax.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "user")
 public class User {
@@ -10,4 +17,29 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idx;
 
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime regdate;
+    private int activated;
+    private String role;
+
+    @Builder
+    public User(Long idx, String name, String email, String password, LocalDateTime regdate, int activated, String role) {
+        this.idx = idx;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.regdate = regdate;
+        this.activated = activated;
+        this.role = role;
+    }
+
+    public void update(String name, String email, String password, int activated, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.activated = activated;
+        this.role = role;
+    }
 }
