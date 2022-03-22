41fc9e071df07e7777a51b388ab2f25678969050
inhwanK <inhan1009@naver.com>
2022-03-23 03:02:59 +0900

    Test(UserRepositoryTest) : 테스트에 필요한 클래스 테스트 환경으로 복사

---
+++ src/test/java/org/deco/gachicoding/domain/user/User.java
@@ -0,0 +1,56 @@
+package org.deco.gachicoding.domain.user;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@DynamicInsert
+@Entity
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Nullable
+    private String name;
+    @Nullable
+    private String email;
+    private String password;
+    private LocalDateTime regdate;
+    private int activated;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(Long idx, String name, String email, String password, LocalDateTime regdate, int activated, Role role) {
+        this.idx = idx;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.regdate = regdate;
+        this.activated = activated;
+        this.role = role;
+    }
+
+    public User socialUpdate(String name, String email) {
+        this.name = name;
+        this.email = email;
+        return this;
+    }
+
+    public User update(String name, String email, String password, int activated, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.activated = activated;
+        this.role = role;
+        return this;
+    }
+}
