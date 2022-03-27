4913ffd5cdf63e5983802b53dfba8e6e59ef47e7
inhwanK <inhan1009@naver.com>
2022-03-28 02:34:07 +0900

    Test(UserServiceTest2) : 테스트 코드 인환, 영준 분리

--- src/test/java/org/deco/gachicoding/service/user/UserServiceTest.java
+++ src/test/java/org/deco/gachicoding/service/user/UserServiceTest.java
@@ -1,86 +1,23 @@
 package org.deco.gachicoding.service.user;
 
-import org.deco.gachicoding.domain.user.User;
-import org.deco.gachicoding.dto.user.JwtRequestDto;
-import org.deco.gachicoding.dto.user.JwtResponseDto;
 import org.deco.gachicoding.dto.user.UserSaveRequestDto;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // 서비스 테스트에서 비즈니스 로직에서 발생할 수 있는 예외 상황의 테스트를 진행한다
-//@ExtendWith(SpringExtension.class)
-
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserServiceTest {
 
     @Autowired
     UserService userService;
-
-    @AfterEach
-    void cleanUp() {
-
-    }
-
-    @Test
-    void 이메일로_유저정보_가져오기_해당유저존재() {
-        String email = "test@test.com";
-        Optional<User> user = userService.getUserByEmail(email);
-        assertTrue(user.isPresent());
-    }
-
-    @Test
-    void 이메일로_유저정보_가져오기_해당유저없음() {
-        String email = "inhan1009@naver.com";
-        Optional<User> user = userService.getUserByEmail(email);
-        assertTrue(user.isEmpty());
-    }
-
-
-    @Test
-    void 중복이메일_존재() {
-
-        String email = "test@test.com";
-        assertTrue(userService.existDuplicateEmail(email));
-    }
-
-    @Test
-    void 중복이메일_존재하지_않음() {
-
-        String email = "inhan1009@naver.com";
-        assertFalse(userService.existDuplicateEmail(email));
-    }
-
-    @Test
-    void 회원가입_성공() {
-
-        String email = "inhan1009@naver.com";
-        String name = "김인환";
-        String password = "1234";
-
-        UserSaveRequestDto dto = new UserSaveRequestDto(name, email, password);
-
-        userService.registerUser(dto);
-
-        Optional<User> user = userService.getUserByEmail(email);
-
-        assertEquals(email, user.get().getUserEmail());
-        assertEquals(name, user.get().getUserName());
-        assertEquals(password, user.get().getUserPassword());
-    }
-
-    @Test
-    void 회원가입_실패_이메일_중복인_경우() {
-
-    }
 
     @Test
     @DisplayName("UserService - JWT 로그인 테스트")
